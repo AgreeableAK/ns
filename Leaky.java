5c052cdaa67b718109d60a8827fe6a60ef31f28e
AK <88891926+AgreeableAK@users.noreply.github.com>
2024-12-09 18:09:29 +0530

    Update Leaky.java

--- Leaky.java
+++ Leaky.java
@@ -1,45 +1,42 @@
 import java.util.Scanner;
 
 class Queue {
-    int[] q; // Array to store packets
-    int f = 0, r = 0, size; // f: front, r: rear, size of the queue
+    int[] q;
+    int f = 0, r = 0, size;
 
-    // Constructor to initialize the queue
     Queue(int size) {
         this.size = size;
         q = new int[size];
     }
 
-    // Method to insert packets into the queue
     void insert(int n) {
         Scanner in = new Scanner(System.in);
         for (int i = 0; i < n; i++) {
             System.out.print("\nEnter element " + (i + 1) + ": ");
             int ele = in.nextInt();
 
-            if (r >= size) { // Check if the queue is full
+            if (r >= size) {
                 System.out.println("\nQueue is full. Lost Packet: " + ele);
                 break;
             } else {
-                q[r] = ele; // Add the packet to the queue
-                r++; // Move the rear pointer
+                q[r] = ele;
+                r++;
             }
         }
     }
 
-    // Method to delete packets (leak packets)
     void delete() {
-        if (r == f) { // Check if the queue is empty
+        if (r == f) {
             System.out.println("\nQueue empty. No packets to leak.");
         } else {
-            for (int i = f; i < r; i++) { // Leak packets one by one
+            for (int i = f; i < r; i++) {
                 try {
-                    Thread.sleep(1000); // Simulate a 1-second delay between leaks
+                    Thread.sleep(1000);
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
                 System.out.println("\nLeaked Packet: " + q[f]);
-                f++; // Move the front pointer
+                f++;
             }
         }
     }
@@ -49,20 +46,15 @@ public class Leaky extends Thread {
     public static void main(String[] args) {
         Scanner src = new Scanner(System.in);
 
-        // Input: Queue size and number of packets to send
         System.out.println("Enter the size of the queue:");
         int size = src.nextInt();
         System.out.println("Enter the number of packets to be sent:");
         int n = src.nextInt();
 
-        // Create a queue of the specified size
         Queue q = new Queue(size);
-
-        // Insert and delete packets
         q.insert(n);
         q.delete();
 
         src.close();
     }
 }
-
